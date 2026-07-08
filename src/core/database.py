9987952ac851f3d195e6dece2dd93da2e136705f
Daniel Ogbuike <ogbuikedaniel@gmail.com>
2026-07-08 11:15:04 +0100

    feat: implement SQLAlchemy async database engine and session dependency provider

---
+++ src/core/database.py
@@ -0,0 +1,32 @@
+from sqlalchemy.ext.asyncio import (
+    AsyncSession,
+    async_sessionmaker,
+    create_async_engine,
+    AsyncEngine,
+)
+from sqlalchemy.orm import DeclarativeBase
+from typing import AsyncGenerator
+from src.core.config import settings
+
+class Base(DeclarativeBase):
+    pass
+
+engine = create_async_engine(
+    url=settings.DB_URL,
+    echo=False,
+    future=True
+)
+
+AsyncSessionLocal = async_sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False,
+    autoflush=False
+)
+
+async def get_db():
+    async with AsyncSessionLocal() as session:
+        try:
+            yield session
+        finally:
+            await session.close()
