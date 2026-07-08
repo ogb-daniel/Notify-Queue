7ac73c3ec707140d1b20b1939167e7637f646d4b
Daniel Ogbuike <ogbuikedaniel@gmail.com>
2026-07-08 20:38:07 +0100

    feat: initial redis in lifespan

--- src/main.py
+++ src/main.py
@@ -1,12 +1,14 @@
+
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from typing import AsyncIterator
 from src.core.config import settings
 from src.core.database import engine, Base
 from src.api.v1.api import router as api_router
+from src.core.redis import init_redis, close_redis
 import logging
 import sys
-from src.core.redis import init_redis, close_redis
+
 
 def setup_logging() -> None:
     logging.basicConfig(
@@ -15,6 +17,7 @@ def setup_logging() -> None:
         datefmt="%Y-%m-%d %H:%M:%S",
         handlers=[logging.StreamHandler(sys.stdout)],
     )
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI) -> AsyncIterator[None]:
@@ -25,12 +28,13 @@ async def lifespan(app: FastAPI) -> AsyncIterator[None]:
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
 
-    init_redis()
+    await init_redis()
     logger.info("Database + Redis initialized")
+
     yield
+
     await close_redis()
     logger.info("NotifyQueue API shutdown complete")
-
 
 
 app = FastAPI(
