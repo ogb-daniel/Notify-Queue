82ec91088d16dffb705bddbe4aab6acc9b078e40
Daniel Ogbuike <ogbuikedaniel@gmail.com>
2026-07-08 20:42:37 +0100

    refactor: add typings to database impls

--- src/core/database.py
+++ src/core/database.py
@@ -2,29 +2,31 @@ from sqlalchemy.ext.asyncio import (
     AsyncSession,
     async_sessionmaker,
     create_async_engine,
-    AsyncEngine,
 )
 from sqlalchemy.orm import DeclarativeBase
 from typing import AsyncGenerator
 from src.core.config import settings
 
+
 class Base(DeclarativeBase):
     pass
+
 
 engine = create_async_engine(
     url=settings.DB_URL,
     echo=False,
-    future=True
+    future=True,
 )
 
 AsyncSessionLocal = async_sessionmaker(
     bind=engine,
     class_=AsyncSession,
     expire_on_commit=False,
-    autoflush=False
+    autoflush=False,
 )
 
-async def get_db():
+
+async def get_db() -> AsyncGenerator[AsyncSession, None]:
     async with AsyncSessionLocal() as session:
         try:
             yield session
